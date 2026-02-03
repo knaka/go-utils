79e393035b3fb1d3179702c6610c1d0e42587e4f
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2026-02-03 10:31:35 +0900

    chore: remove puhsing dir

--- project.lib.sh
+++ project.lib.sh
@@ -2,10 +2,8 @@
 # shellcheck shell=sh
 "${sourced_b5282d2-false}" && return 0; sourced_b5282d2=true
 
-set -- "$PWD" "${0%/*}" "$@"; if test "$2" != "$0"; then cd "$2" 2>/dev/null || :; fi
 . ./task.sh
 . ./go.lib.sh
-cd "$1"; shift 2
 
 # Run tests.
 task_test() {
